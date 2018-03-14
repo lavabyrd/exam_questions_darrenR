104bdc48432547542c361d832a81d4f9abf84295
splinterific <markgpreston@gmail.com>
2018-03-14 20:08:43 +0000

    added Q2

---
+++ Q2/CollegeRegistration/CollegeRegistration.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Q2
+{
+    partial class CollegeRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtOrg = new System.Windows.Forms.TextBox();
+            this.txtConfID = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 206);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 327);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Organization";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 448);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Conference ID";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(250, 79);
+            this.txtFName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(248, 31);
+            this.txtFName.TabIndex = 4;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(250, 192);
+            this.txtSurname.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(248, 31);
+            this.txtSurname.TabIndex = 5;
+            // 
+            // txtOrg
+            // 
+            this.txtOrg.Location = new System.Drawing.Point(250, 321);
+            this.txtOrg.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtOrg.Name = "txtOrg";
+            this.txtOrg.Size = new System.Drawing.Size(248, 31);
+            this.txtOrg.TabIndex = 6;
+            // 
+            // txtConfID
+            // 
+            this.txtConfID.Location = new System.Drawing.Point(250, 442);
+            this.txtConfID.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtConfID.Name = "txtConfID";
+            this.txtConfID.Size = new System.Drawing.Size(248, 31);
+            this.txtConfID.TabIndex = 7;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(352, 554);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(150, 44);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 656);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtConfID);
+            this.Controls.Add(this.txtOrg);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "ABC College Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtOrg;
+        private System.Windows.Forms.TextBox txtConfID;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
+
---
+++ Q2/CollegeRegistration/CollegeRegistration.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Q2
+{
+    public partial class CollegeRegistration : Form
+    {
+        readonly int FirstNameMinLength = 2;
+        readonly int SurnameMinLength = 2;
+
+        static readonly string FILE_PATH = @"C:\AdvProg\registration.bin";
+
+        string ConfIDRegEx;
+
+        public CollegeRegistration()
+        {
+            InitializeComponent();
+
+            this.Text = "Conference Registration";
+            ConfIDRegEx = ConfigurationManager.AppSettings["ConfID"];
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if(!ValidateConfIDField())
+            {
+                MessageBox.Show("Conference ID field is invalid. Registration Failed");
+                txtConfID.Focus();
+            }
+            else
+            {
+                // validate firstname and surname are both 2 chars+ in length
+                bool fNameValid = ValidateFirstName(), surnameValid = ValidateSurname();
+
+                if(!fNameValid || !surnameValid)
+                {
+                    string msg;
+                    if(!fNameValid && !surnameValid)
+                    {
+                        msg = "Firstname and surname are invalid";
+                        txtSurname.Focus();
+                    }
+                    else if(!fNameValid)
+                    {
+                        msg = "Firstname is invalid";
+                        txtFName.Focus();
+                    }
+                    else
+                    {
+                        msg = "Surname is invalid";
+                        txtSurname.Focus();
+                    }
+
+                    MessageBox.Show(msg);
+                }
+                else
+                {
+                    SaveRegistrationInfo();
+                }
+            }
+        }
+
+        private bool ValidateSurname()
+        {
+            return txtSurname.Text.Length >= SurnameMinLength;
+        }
+
+        private bool ValidateFirstName()
+        {
+            return txtFName.Text.Length >= FirstNameMinLength;
+        }
+
+        private void SaveRegistrationInfo()
+        {
+            using (Stream fs = File.Open(FILE_PATH, FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter writer = new BinaryWriter(fs))
+                {
+                    try
+                    {
+                        writer.Write(txtFName.Text);
+                        writer.Write(txtSurname.Text);
+                        writer.Write(txtOrg.Text);
+                        writer.Write(txtConfID.Text);
+                        MessageBox.Show("Binary file created and written to successfully");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private bool ValidateConfIDField()
+        {
+            bool confIDIsValid = false;
+
+            string confID = txtConfID.Text;
+
+            if (Regex.IsMatch(confID, ConfIDRegEx))
+            {
+                confIDIsValid = true;
+            }
+
+            return confIDIsValid;
+        }
+    }
+}
+
